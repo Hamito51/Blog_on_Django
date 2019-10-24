5e52c0e657f804e91ddba6d7495f4a748cec1fd1
Hamito51 <chuley.plc@gmail.com>
2019-10-24 16:31:39 +0300

    Created views and urls

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 	
 # Create your models here.
+class PublishedManager(models.Manager):
+		def get_queryset(self):
+			return super().get_queryset().filter(status='published')
+				
+
 class Post(models.Model):
 	"""Модель данных для статей блога"""
 	STATUS_CHOICES = (
@@ -19,11 +25,17 @@ class Post(models.Model):
 	updated = models.DateTimeField(auto_now=True)
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+	objects = models.Manager() # Default manager
+	published = PublishedManager() # New manager
+
 	class Meta:
 		ordering = ('-publish',)
+
+	def get_absolute_url(self):
+			return reverse('blog:post_detail', args=[self.publish.year, 
+							self.publish.month, self.publish.day, self.slug])	
 
 	def __str__(self):
 			return self.title
 
 
-		
