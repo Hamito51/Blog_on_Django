7c39ebf137c889251032795fd71d4a29e82f8b18
Hamito51 <chuley.plc@gmail.com>
2019-11-01 16:09:12 +0300

    Added pagination and templates

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -31,11 +31,9 @@ class Post(models.Model):
 	class Meta:
 		ordering = ('-publish',)
 
-	def get_absolute_url(self):
-			return reverse('blog:post_detail', args=[self.publish.year, 
-							self.publish.month, self.publish.day, self.slug])	
-
+	
 	def __str__(self):
-			return self.title
+		return self.title
 
-
+	def get_absolute_url(self):
+		return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
