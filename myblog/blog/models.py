6cb9d877080e427e4ecceecb98139f8bb41df437
Hamito51 <chuley.plc@gmail.com>
2019-11-21 13:30:13 +0300

    Added field tags

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 	
 # Create your models here.
@@ -27,6 +28,8 @@ class Post(models.Model):
 
 	objects = models.Manager() # Default manager
 	published = PublishedManager() # New manager
+
+	tags = TaggableManager()
 
 	class Meta:
 		ordering = ('-publish',)
